ddcf013e4ec07c541ee5fe485f801d87a6bb5347
mechakin <barrientosryan83@gmail.com>
2023-04-05 20:53:26 -0400

    refactor styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <h1 className="text-4xl">this is a website</h1>
-    </>
-  );
+  return <h1 className="text-4xl">this is a website</h1>;
 };
 
 export default Home;
