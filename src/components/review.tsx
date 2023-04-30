fa5a605c24390bd2ee18a4bbde2125118d787243
mechakin <barrientosryan83@gmail.com>
2023-04-30 16:07:26 -0400

    add exit button

--- src/components/review.tsx
+++ src/components/review.tsx
@@ -13,6 +13,7 @@ import toast from "react-hot-toast";
 import { createPortal } from "react-dom";
 import { Modal } from "./modal";
 import dayjs from "dayjs";
+import { ExitButton } from "./exit-button";
 
 type ReviewWithUser =
   RouterOutputs["review"]["getReviewsByGameId"]["reviews"][number];
@@ -122,26 +123,7 @@ export default function Review(props: ReviewWithUser) {
                 </span>
               </div>
               <button onClick={handleUpdateModal} className="text-2xl">
-                <svg
-                  height="20px"
-                  width="20px"
-                  version="1.1"
-                  id="Capa_1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  viewBox="0 0 460.775 460.775"
-                  xmlSpace="preserve"
-                  className="fill-zinc-400 transition duration-75 hover:fill-zinc-100"
-                >
-                  <path
-                    d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
-	c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
-	c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
-	c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
-	l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
-	c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
-                  />
-                </svg>
+                <ExitButton />
               </button>
             </div>
             <div className="sm:flex">
@@ -188,26 +170,7 @@ export default function Review(props: ReviewWithUser) {
                   </span>
                 </div>
                 <button onClick={handleUpdateModal} className="text-2xl">
-                  <svg
-                    height="20px"
-                    width="20px"
-                    version="1.1"
-                    id="Capa_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 460.775 460.775"
-                    xmlSpace="preserve"
-                    className="fill-zinc-400 transition duration-75 hover:fill-zinc-100"
-                  >
-                    <path
-                      d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
-	c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
-	c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
-	c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
-	l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
-	c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
-                    />
-                  </svg>
+                  <ExitButton />
                 </button>
               </div>
               <div className="sm:flex">
@@ -311,26 +274,7 @@ export default function Review(props: ReviewWithUser) {
             className="flex w-full min-w-fit flex-col items-end pt-2"
             onClick={handleDeleteModal}
           >
-            <svg
-              height="20px"
-              width="20px"
-              version="1.1"
-              id="Capa_1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 460.775 460.775"
-              xmlSpace="preserve"
-              className="fill-zinc-400 transition duration-75 hover:fill-zinc-100"
-            >
-              <path
-                d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
-c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
-c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
-c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
-l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
-c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
-              />
-            </svg>
+            <ExitButton />
           </button>
         )}
       </div>
